7c176b202f20e4d482128bfc82b31832714ea9c2
Andrea Bergonzi <andrea.bergonzi@live.it>
2026-03-17 17:03:35 +0100

    Integrate published exocor package

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { createRoot } from 'react-dom/client';
+import { SpatialProvider } from 'exocor';
 import App from './app/App.tsx';
 import './styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <App />
+  <SpatialProvider>
+    <App />
+  </SpatialProvider>
 );
